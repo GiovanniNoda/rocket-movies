e3dfaa75151b42b98d08dfb3b0ddf1fdafe7e0bd
Giovanni <giovanniminoru@gmail.com>
2024-08-17 00:19:29 -0300

    esqueleto finalizado

--- rocketmovies/src/main.jsx
+++ rocketmovies/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
+import { Home } from './pages/Home/index.jsx'
+
+import theme from './styles/theme'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ rocketmovies/src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+    COLORS: {
+      BACKGROUND_BODY: "#1C1B1E",
+      BACKGROUND_PINK: "rgba(255, 133, 155, 0.05)",
+      BACKGROUND_INPUT: "#262529",
+  
+      WHITE: "#F4EDE8",
+      PINK: "#FF859B",
+  
+      GRAY_100: "#CAC4CF",
+      GRAY_200: "#948F99",
+      GRAY_300: "#312E38",
+      GRAY_400: "#282124",
+      GRAY_500: "#0D0C0F"
+    }
+  }
